a631278e569ddb91f757e4ae1789e4e759a1353e
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2023-09-22 23:38:53 +0500

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.decorator;
+
+/**
+ *
+ * @author skytech laptop store
+ */
+public interface Shape {
+   void draw();
+}
