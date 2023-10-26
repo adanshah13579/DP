21a4fb3284328dc0b10b2e7c17da2c750056c8cf
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2023-10-26 10:15:58 +0500

    update

---
+++ Builder/src/builder/Packing.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package builder;
+
+/**
+ *
+ * @author fa20-bse-036
+ */
+public interface Packing {
+   public String pack();
+}
