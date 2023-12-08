f2b187a42e9c01d44a6018f1efa0bee2ef676eb9
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2023-12-09 01:03:46 +0500

    update

---
+++ flyweigth/src/flyweigth/FlyweightPatternDemo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flyweigth;
+
+/**
+ *
+ * @author skytech laptop store
+ */
+public class FlyweightPatternDemo {
+   private static final String colors[] = { "Red", "Green", "Blue", "White", "Black" };
+   public static void main(String[] args) {
+
+      for(int i=0; i < 20; ++i) {
+         Circle circle = (Circle)ShapeFactory.getCircle(getRandomColor());
+         circle.setX(getRandomX());
+         circle.setY(getRandomY());
+         circle.setRadius(100);
+         circle.draw();
+      }
+   }
+   private static String getRandomColor() {
+      return colors[(int)(Math.random()*colors.length)];
+   }
+   private static int getRandomX() {
+      return (int)(Math.random()*100 );
+   }
+   private static int getRandomY() {
+      return (int)(Math.random()*100);
+   }
+}
---
+++ flyweigth/src/flyweigth/ShapeFactory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flyweigth;
+
+/**
+ *
+ * @author skytech laptop store
+ */
+import java.util.HashMap;
+
+public class ShapeFactory {
+
+   // Uncomment the compiler directive line and
+   // javac *.java will compile properly.
+   // @SuppressWarnings("unchecked")
+   private static final HashMap circleMap = new HashMap();
+
+   public static Shape getCircle(String color) {
+      Circle circle = (Circle)circleMap.get(color);
+
+      if(circle == null) {
+         circle = new Circle(color);
+         circleMap.put(color, circle);
+         System.out.println("Creating circle of color : " + color);
+      }
+      return circle;
+   }
+}
