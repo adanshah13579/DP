524d4bd57ca9c1511c5ae542fda20ccd706a9dea
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2023-12-09 01:32:17 +0500

    update

---
+++ State/src/state/StartState.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package state;
+
+/**
+ *
+ * @author skytech laptop store
+ */
+public class StartState implements State {
+
+   public void doAction(Context context) {
+      System.out.println("Player is in start state");
+      context.setState(this);	
+   }
+
+   public String toString(){
+      return "Start State";
+   }
+}
