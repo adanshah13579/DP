dc23880b9b2086d91fef04499a34377a47678e7e
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2023-12-14 09:24:30 +0500

    update

---
+++ interpreter/src/interpreter/AndExpression.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interpreter;
+
+/**
+ *
+ * @author skytech laptop store
+ */
+public class AndExpression implements Expression {
+	 
+   private Expression expr1 = null;
+   private Expression expr2 = null;
+    private Expression expr3 = null;
+   public AndExpression(Expression expr1, Expression expr2) { 
+      this.expr1 = expr1;
+      this.expr2 = expr2;
+      this.expr3 = expr3;
+   }
+
+   @Override
+   public boolean interpret(String context) {		
+      return expr1.interpret(context) && expr2.interpret(context) && expr3.interpret(context) ;
+   }
+}
